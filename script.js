48005f47292920e4904ed194a268d3114c4fca8f
Jesus Antonio Lopez Bandala <lopezbandalajesusantonio@gmail.com>
2024-05-29 17:39:28 -0600

    formatStr

--- script.js
+++ script.js
@@ -7,28 +7,36 @@ const zone = document.getElementById("zone");
 const colonys = document.getElementById("colonys");
 const submitForm = document.getElementById("submit-form");
 
-function disableSubmit() {
-  if (
-    CP.value === "" ||
-    state.value === "" ||
-    state.value === null ||
-    zone.value === ""
-  )
+const disableSubmit = () => {
+  if (CP.value === "" || state.value === "" || zone.value === "")
     submitForm.disabled = true;
   else submitForm.disabled = false;
-}
+};
 
-document.addEventListener("DOMContentLoaded", () => {
-  disableSubmit();
-});
+const formatStr = (str) =>
+  str
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toUpperCase();
 
-CP.addEventListener("input", function (e) {
+const cleanForm = () => {
+  state.value = "";
+  zone.value = "";
+  colonys.innerHTML = "";
+};
+
+document.addEventListener("DOMContentLoaded", () => disableSubmit());
+
+CP.addEventListener("keypress", function (e) {
   //Hacemos que el campo de CP se limite a solo números de 5 dígitos
   let char = e.key;
   if (!/[0-9]/.test(char) || this.value.length == 5) {
     e.preventDefault();
   }
+});
 
+//Aqui empiza la magia
+CP.addEventListener("input", function (e) {
   //Al ingresar 5 dígitos, hacemos una petición al servidor y mostramos la información
   if (this.value.length === 5) {
     console.log("Buscando en la API el CP " + this.value);
@@ -41,25 +49,23 @@ CP.addEventListener("input", function (e) {
         // Si no se encontraron códigos postales
         if (data.zip_codes.length === 0) {
           alert("No se encontró el código postal");
-          state.value = "";
-          zone.value = "";
-          colonys.innerHTML = "";
+          cleanForm();
           disableSubmit();
           return;
         }
 
         data.zip_codes.forEach((zip_code) => {
-          state.value = zip_code.d_estado;
-          zone.value = zip_code.d_mnpio;
+          state.value = formatStr(zip_code.d_estado);
+          zone.value = formatStr(zip_code.d_mnpio);
           //Rellenamos la info de las colonias
           let option = document.createElement("option");
-          option.text = option.value = zip_code.d_asenta;
+          option.text = option.value = formatStr(zip_code.d_asenta);
           colonys.appendChild(option);
         });
         disableSubmit();
       });
+  } else {
+    cleanForm();
+    disableSubmit();
   }
 });
-
-// if(CP)
-// fetch(`${API_URL}?zip_code=${CP.textContent}`)
