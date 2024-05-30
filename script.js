e534efb47e4ede390b4107aa911c9e2c82d52c69
Jesus Antonio Lopez Bandala <lopezbandalajesusantonio@gmail.com>
2024-05-29 18:17:24 -0600

    Control de los numeros interiores y exteriores

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ const state = document.getElementById("state");
 const zone = document.getElementById("zone");
 const colonys = document.getElementById("colonys");
 const submitForm = document.getElementById("submit-form");
+const street = document.getElementById("street");
+const extNum = document.getElementById("ext-num");
+const inNum = document.getElementById("in-num");
+
+const MAX_LENGTH_NUMS = 10;
 
 const disableSubmit = () => {
-  if (CP.value === "" || state.value === "" || zone.value === "")
+  if (
+    CP.value === "" ||
+    state.value === "" ||
+    zone.value === "" ||
+    street.value === ""
+  )
     submitForm.disabled = true;
   else submitForm.disabled = false;
 };
@@ -25,14 +35,56 @@ const cleanForm = () => {
   colonys.innerHTML = "";
 };
 
+const makeFieldsRequired = () => {
+  if (extNum.value || inNum.value) {
+    extNum.required = true;
+    inNum.required = true;
+  } else {
+    extNum.required = false;
+    inNum.required = false;
+  }
+};
+
 document.addEventListener("DOMContentLoaded", () => disableSubmit());
 
+//Hacemos que el campo de CP se limite a solo números de 5 dígitos
 CP.addEventListener("keypress", function (e) {
-  //Hacemos que el campo de CP se limite a solo números de 5 dígitos
   let char = e.key;
   if (!/[0-9]/.test(char) || this.value.length == 5) {
     e.preventDefault();
   }
+});
+
+//Hacemos que el campo de calle tenga el formato adecuado
+street.addEventListener("input", function (e) {
+  this.value = formatStr(this.value);
+  disableSubmit(); //Verificamos que no esté vacío el campo de la calle
+});
+
+//Hacemos que el campo de número exterior se limite a solo 5 caracteres
+extNum.addEventListener("keypress", function (e) {
+  if (this.value.length == MAX_LENGTH_NUMS) {
+    e.preventDefault();
+  }
+});
+
+//Hacemos que el campo de número exterior tenga el formato adecuado
+extNum.addEventListener("input", function (e) {
+  this.value = formatStr(this.value);
+  makeFieldsRequired();
+});
+
+//Hacemos que el campo de número interior se limite a solo 5 caracteres
+inNum.addEventListener("keypress", function (e) {
+  if (this.value.length == MAX_LENGTH_NUMS) {
+    e.preventDefault();
+  }
+});
+
+//Hacemos que el campo de número interior tenga el formato adecuado
+inNum.addEventListener("input", function (e) {
+  this.value = formatStr(this.value);
+  makeFieldsRequired();
 });
 
 //Aqui empiza la magia
